8a7bbb7453e6d3eb86ed10421d3f034e962fd596
Kchar <38297595+kevinlichang@users.noreplay.github.com>
2018-10-08 20:56:27 -0400

    added beginning code

--- liri.js
+++ liri.js
@@ -1,1 +1,10 @@
+const request = require("request");
+const Spotify = require("node-spotify-api");
+const moment = require("moment");
+
 require("dotenv").config();
+
+const spotifyKeys = require("./keys.js");
+
+// turn on new spotify app
+const spotify = new Spotify(spotifyKeys.spotify);
