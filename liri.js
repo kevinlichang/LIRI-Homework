04f5babea519d98199993b132936bae83dbc427a
Kchar <38297595+kevinlichang@users.noreplay.github.com>
2018-10-09 22:45:24 -0400

    added concert-this command

--- liri.js
+++ liri.js
@@ -7,13 +7,43 @@ require("dotenv").config();
 const spotifyKeys = require("./keys.js");
 
 const nodeCommand = process.argv[2];
-const nodeNameInput = process.argv[3];
+const nodeArgs = process.argv;
+let name = "";
+
+function getName() {
+  for (let i = 3; i < nodeArgs.length; i++) {
+    if (i > 3 && i < nodeArgs.length) {
+      name = name + "+" + nodeArgs[i];
+    } else {
+      name += nodeArgs[i];
+    }
+  }
+};
 
 // turn on new spotify app
 const spotify = new Spotify(spotifyKeys.spotify);
 
 
 // Command line concert-this <artist/band name>
+function bandsInTown() {
+  getName();
+
+  const queryUrl = `https://rest.bandsintown.com/artists/${name}/events?app_id=codingbootcamp`
+
+  request(queryUrl, function (error, response, body) {
+    const result = JSON.parse(body);
+    if (!error && response.statusCode === 200) {
+      for (let i = 0; i < result.length; i++) {
+        console.log(`
+        ================================================================
+        Venue Name: ${result[i].venue.name}
+        Location: ${result[i].venue.city}, ${result[i].venue.region}, ${result[i].venue.country}
+        Event Date: ${result[i].datetime}
+        ================================================================`)
+      }
+    };
+  });
+};
 
 
 // spotify-this-song <song name>
@@ -21,44 +51,38 @@ const spotify = new Spotify(spotifyKeys.spotify);
 
 // movie-this <movie-name>
 function movie() {
-  const nodeArgs = process.argv;
-  let movieName = "";
-  for (var i = 3; i < nodeArgs.length; i++) {
-
-    if (i > 3 && i < nodeArgs.length) {
-      movieName = movieName + "+" + nodeArgs[i];   
-    } 
-    else {
-      movieName += nodeArgs[i]; 
-    }
-  }
+  getName();
 
   if (nodeArgs.length < 4) {
-    movieName = "Mr. Nobody";
+    name = "Mr. Nobody";
   }
-  
-  const queryUrl = `http://www.omdbapi.com/?t=${movieName}&apikey=trilogy`;
 
-  request(queryUrl, function(error, response, body) {
+  const queryUrl = `http://www.omdbapi.com/?t=${name}&apikey=trilogy`;
+
+  request(queryUrl, function (error, response, body) {
+    const result = JSON.parse(body);
     if (!error && response.statusCode === 200) {
       console.log(`
-        ===============================================
-        Title: ${JSON.parse(body).Title}
-        Year: ${JSON.parse(body).Year}
-        IMDB Rating: ${JSON.parse(body).imdbRating}
-        Rotten Tomatoes Rating: ${JSON.parse(body).Ratings[1].Value}
-        Country: ${JSON.parse(body).Country}
-        Language: ${JSON.parse(body).Language}
-        Plot: ${JSON.parse(body).Plot}
-        Actors: ${JSON.parse(body).Actors}
-        ===============================================`
-      )
+        ================================================================
+        Title: ${result.Title}
+        Year: ${result.Year}
+        IMDB Rating: ${result.imdbRating}
+        Rotten Tomatoes Rating: ${result.Ratings[1].Value}
+        Country: ${result.Country}
+        Language: ${result.Language}
+        Plot: ${result.Plot}
+        Actors: ${result.Actors}
+        ================================================================`)
     };
   });
 };
 
 switch (nodeCommand) {
+  case "concert-this":
+    bandsInTown();
+    break;
   case "movie-name":
     movie();
     break;
+
 };
